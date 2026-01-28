823b9254de4a66718f844e850147dd77c4e8da3f
Rajat Gohil <rgohil27@gmail.com>
2026-01-28 20:54:02 +0530

    Sync TMDB genres on startup\

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Syncing genres from TMDB..."
+python manage.py sync_genres || true
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
