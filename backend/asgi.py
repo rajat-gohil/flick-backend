6e93c775d976820fad0501542616dd9e9b9abf08
Rajat Gohil <rgohil27@gmail.com>
2026-01-29 16:23:35 +0530

    Allow unauthenticated WebSocket connections for session events

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,23 +1,15 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import core.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(core.routing.websocket_urlpatterns)
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        __import__("core.routing").routing.websocket_urlpatterns
     ),
 })
