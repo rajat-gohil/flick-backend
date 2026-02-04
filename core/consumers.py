ab8e9812897fa57f4a668d054b92af7bcba0da98
Rajat Gohil <rgohil27@gmail.com>
2026-02-04 17:22:39 +0530

    Broadcast partner disconnected event over WebSocket

--- core/consumers.py
+++ core/consumers.py
@@ -15,6 +15,15 @@ class MatchConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Notify other participant that someone disconnected
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "partner_disconnected",
+                "channel": self.channel_name,
+            }
+        )
+
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
@@ -34,8 +43,19 @@ class MatchConsumer(AsyncWebsocketConsumer):
             "type": "session_ended",
             "session_id": event["session_id"],
         }))
+
+
     async def swipe_event(self, event):
         await self.send(text_data=json.dumps({
             "type": "partner_swiping",
             "user_id": event["user_id"],
+        }))
+
+    async def partner_disconnected(self, event):
+    # Do not notify the user who disconnected
+        if self.channel_name == event.get("channel"):
+            return
+
+        await self.send(text_data=json.dumps({
+            "type": "partner_disconnected",
         }))
